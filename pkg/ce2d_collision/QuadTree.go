33bdd30526cb6d8ce3731a357794d6ff32d117b3
Lirik-Keltran <keltran.987@gmail.com>
2021-08-08 21:07:22 +1100

    pre-pre-pre-pre-pre-pre-pre-pre-beta

--- pkg/ce2d_collision/QuadTree.go
+++ pkg/ce2d_collision/QuadTree.go
@@ -5,19 +5,65 @@ import (
 )
 
 type QuadTree struct {
-	Position      ce2d_math.Vector2D
-	Width, Height int
-	Parent        *QuadTree
-	Deep          int
-	Nodes         [4]*QuadTree
-	Coliders      []AABB
+	MinPosition, MaxPosition *ce2d_math.Vector2D
+	Nodes                    [4]*QuadTree
+	Coliders                 []AABB
+	Deep                     int
 }
 
-func CreateQuadTree(x, y float64, width, height int) *QuadTree {
-
-	return &QuadTree{}
+func CreateQuadTree(x, y, width, height float64, deep int) *QuadTree {
+	return &QuadTree{
+		MinPosition: ce2d_math.CreateVector2D(x, y),
+		MaxPosition: ce2d_math.CreateVector2D(x+width, y+height),
+		Deep:        0,
+	}
 }
 
-func CreateEmptyQuadTree(width, height int) *QuadTree {
-	return &QuadTree{}
+func (q *QuadTree) addNodes() {
+	q.Nodes[0] = CreateQuadTree(
+		q.MinPosition.X, q.MinPosition.Y,
+		q.MaxPosition.X/2, q.MaxPosition.Y/2, q.Deep+1)
+	q.Nodes[1] = CreateQuadTree(
+		q.MaxPosition.X/2, q.MinPosition.Y,
+		q.MaxPosition.X, q.MaxPosition.Y/2, q.Deep+1)
+	q.Nodes[2] = CreateQuadTree(
+		q.MinPosition.X, q.MaxPosition.Y/2,
+		q.MaxPosition.X/2, q.MaxPosition.Y, q.Deep+1)
+	q.Nodes[3] = CreateQuadTree(
+		q.MaxPosition.X/2, q.MaxPosition.Y/2,
+		q.MaxPosition.X, q.MaxPosition.Y, q.Deep+1)
+}
+
+func (q *QuadTree) AddColiders(Coliders []AABB) {
+	t := make([]AABB, 0)
+	for _, colider := range Coliders {
+		if dividingAxisCheck(colider.MinPosition, colider.MaxPosition, q.MinPosition, q.MaxPosition) {
+			t = append(t, colider)
+		}
+	}
+	if len(t) > 0 {
+		q.addNodes()
+	}
+	if q.Deep < 5 && len(t) > 2 {
+		for i := 0; i < 4 && q.Nodes[i] != nil; i++ {
+			q.Nodes[i].AddColiders(t)
+		}
+	} else if len(t) > 0 {
+		q.Coliders = append(q.Coliders, t...)
+	}
+
+}
+
+func (q *QuadTree) Start() {
+	if q.Nodes[0] != nil {
+		for _, i := range q.Nodes {
+			i.Start()
+		}
+	} else {
+		for index, item := range q.Coliders {
+			for i := index + 1; i < len(q.Coliders); i++ {
+				item.chek(&q.Coliders[index])
+			}
+		}
+	}
 }
--- pkg/ce2d_collision/Сollision.go
+++ pkg/ce2d_collision/Сollision.go
@@ -5,21 +5,28 @@ import (
 )
 
 type AABB struct {
-	Position      *ce2d_math.Vector2D
-	Width, Height int
-	callback      func(self, other *AABB)
-	condition     func(other *AABB) bool
+	MinPosition, MaxPosition *ce2d_math.Vector2D
+	callback                 func(self, other *AABB)
 }
 
-func CreateAABB(x, y float64, width, height int, callback func(self, other *AABB), condition func(other *AABB) bool) *AABB {
+func CreateAABB(x, y, width, height float64, callback func(self, other *AABB)) *AABB {
 	return &AABB{
-		Position: ce2d_math.CreateVector2D(x, y),
-		Width:    width,
-		Height:   height,
-		callback: callback,
+		MinPosition: ce2d_math.CreateVector2D(x, y),
+		MaxPosition: ce2d_math.CreateVector2D(x+width, y+height),
+		callback:    callback,
 	}
 }
 
-func (aabb *AABB) chek(other *AABB) bool {
-	return aabb.condition(other)
+func dividingAxisCheck(aMinPoint, aMaxPoint, bMinPoint, bMaxPoint *ce2d_math.Vector2D) bool {
+	return aMaxPoint.X < bMinPoint.X ||
+		aMinPoint.X > bMaxPoint.X ||
+		aMaxPoint.Y < bMinPoint.Y ||
+		aMinPoint.Y > bMaxPoint.Y
+}
+
+func (a *AABB) chek(b *AABB) {
+	if dividingAxisCheck(a.MinPosition, a.MaxPosition, b.MinPosition, b.MaxPosition) {
+		a.callback(a, b)
+		b.callback(a, b)
+	}
 }
