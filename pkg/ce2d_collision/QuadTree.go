461a9b2e2439f3585f0ab8a9cd80a8e8dc436b39
Lirik-Keltran <keltran.987@gmail.com>
2021-08-08 16:48:26 +1100

    s

--- pkg/ce2d_collision/QuadTree.go
+++ pkg/ce2d_collision/QuadTree.go
@@ -1,13 +1,20 @@
 package ce2d_collision
 
+import (
+	"github.com/Falldot/ce2d/pkg/ce2d_math"
+)
+
 type QuadTree struct {
-	Parent  *QuadTree
-	Deep    int
-	Rects   [4]*QuadTree
-	Colider []AABB
+	Position      ce2d_math.Vector2D
+	Width, Height int
+	Parent        *QuadTree
+	Deep          int
+	Nodes         [4]*QuadTree
+	Coliders      []AABB
 }
 
-func CreateQuadTree(width, height int) *QuadTree {
+func CreateQuadTree(x, y float64, width, height int) *QuadTree {
+
 	return &QuadTree{}
 }
 
--- pkg/ce2d_collision/Сollision.go
+++ pkg/ce2d_collision/Сollision.go
@@ -7,16 +7,19 @@ import (
 type AABB struct {
 	Position      *ce2d_math.Vector2D
 	Width, Height int
+	callback      func(self, other *AABB)
+	condition     func(other *AABB) bool
 }
 
-func CreateAABB(x, y float64, width, height int) *AABB {
+func CreateAABB(x, y float64, width, height int, callback func(self, other *AABB), condition func(other *AABB) bool) *AABB {
 	return &AABB{
 		Position: ce2d_math.CreateVector2D(x, y),
 		Width:    width,
 		Height:   height,
+		callback: callback,
 	}
 }
 
-func (aabb *AABB) chek(AABB) bool {
-	return true
+func (aabb *AABB) chek(other *AABB) bool {
+	return aabb.condition(other)
 }
