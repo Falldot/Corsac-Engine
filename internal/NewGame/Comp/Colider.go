335cab863cf8a6ac46236447097fc69a13a2aa39
Lirik-Keltran <keltran.987@gmail.com>
2021-08-11 17:58:21 +1100

    colider fix

--- internal/NewGame/Comp/Colider.go
+++ internal/NewGame/Comp/Colider.go
@@ -9,13 +9,17 @@ import (
 type Colider struct {
 	MinPosition, MaxPosition *ce2d_math.Point
 	callback                 func(self, other *Colider)
+	chek                     func(self, other *Colider) bool
+	tag                      string
 }
 
-func CreateColider(x, y, width, height int32, callback func(self, other *Colider)) *Colider {
+func CreateColider(x, y, width, height int32, chek func(self, other *Colider) bool, callback func(self, other *Colider)) *Colider {
 	return &Colider{
 		MinPosition: ce2d_math.CreatePoint(x, y),
 		MaxPosition: ce2d_math.CreatePoint(x+width, y+height),
+		chek:        chek,
 		callback:    callback,
+		tag:         "",
 	}
 }
 
@@ -23,7 +27,19 @@ func (a *Colider) AddCallBack(callback func(self, other *Colider)) {
 	a.callback = callback
 }
 
+func (a *Colider) AddChek(chek func(self, other *Colider) bool) {
+	a.chek = chek
+}
+
 func (a *Colider) Chek(b *Colider) {
+	a.chek(a, b)
+}
+
+func (a *Colider) AddTag(tag string) {
+	a.tag = tag
+}
+
+func BasicChek(a, b *Colider) {
 	if ce2d_collision.DividingAxisCheck(a.MinPosition, a.MaxPosition, b.MinPosition, b.MaxPosition) {
 		a.callback(a, b)
 		b.callback(b, a)
