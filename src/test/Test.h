59f272f26da2558dd465eac85cd8ce3ee368678f
Lirik-Keltran <keltran.987@gmail.com>
2021-11-02 06:08:12 +1100

    Test fixed

--- src/test/Test.h
+++ src/test/Test.h
@@ -1,12 +1,12 @@
 #ifdef TEST_ENABLE
 
-#define Test [=]()mutable
+#define Testing [=]()mutable
 
 #endif
 
 #ifdef TEST_BLOCK
 
-#define TestBlock [=](CorsacTest* assert)mutable
+#define TestBlock [=](Corsac::Block* assert)mutable
 
 #endif
 
@@ -28,272 +28,392 @@
 #include <io.h>
 #include <fcntl.h>
 
-struct CorsacBlock
-{
-	std::function<bool()> test;
-	std::string name;
-	std::string comment;
-	int comment_color;
 
-	CorsacBlock(std::string name, std::function<bool()> func)
+/*
+	@desc - Структура теста.
+
+	@field test 			- Лямбда функция с тестом.
+	@field name 			- Имя теста.
+	@field comment 			- Комментарий к тесту.
+	@field comment_color 	- Цвет комментария.
+*/		
+namespace Corsac{
+
+	struct Test
 	{
-		this->test = func;
-		this->name = name;
-	}
-};
+		std::function<bool()> test;
+		std::string name;
+		std::string comment;
+		
 
-class CorsacTest
-{
-	private:
-		std::string name_block;
-		std::vector<CorsacBlock> tests;
-		std::vector<CorsacTest> blocks;
-		int amount_space;
 		int comment_color;
+		int block_comment;
 
-		void space_print(int amount){
-			for (int i = 0; i <= amount; i++)
-			{
-				std::cout << " ";
-			}
-		}
-
-		CorsacTest* create_block(std::string name)
+		Test(std::string name, std::function<bool()> func)
 		{
-
-			this->blocks.push_back(CorsacTest(name, this->amount_space+3));
-
-			return &this->blocks[this->blocks.size()-1];
+			this->test = func;
+			this->name = name;
 		}
+	};
 
-		void set_color(int color)
-		{
-
-			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-			SetConsoleTextAttribute(hConsole, color);
-		}
-
-		int start_test()
-		{
-			unsigned int start_time = clock();
-			this->set_color(STANDART);
-			this->space_print(this->amount_space);
-			std::cout << this->name_block  << ": "<< std::endl;
-
-			int error = 0;
-
-			for(auto i = 0; i < this->tests.size(); i++)
-			{
-				auto cheked_test = this->tests[i];
-				auto result = cheked_test.test();
-
-				this->space_print(this->amount_space+3);
-
-				if(result){
-					this->set_color(GREEN);
-					std::cout << "\xfb:    ";
-				}
-				else{
-					this->set_color(RED);
-					std::cout << "X:    ";
-					error++;
-				}
-
-				std::cout << cheked_test.name <<std::endl;
-
-				if(cheked_test.comment.size() > 0){
-
-					this->space_print(this->amount_space+4);
-					this->set_color(cheked_test.comment_color);
-					std::cout << "> " << cheked_test.comment << std::endl;
-				}
-			}
-
-			for(auto i = 0; i < this->blocks.size(); i++)
-			{
-				error += this->blocks[i].start_test();
-			}
+	class Block
+	{
+		private:
+			std::string name_block;
+			
+			std::vector<Test> tests;
+			std::vector<Block> blocks;
 
 
-			unsigned int end_time = clock(); 
-			unsigned int search_time = end_time - start_time; 
-			this->set_color(YELLOW);
+			int amount_space;
+			int comment_color;
 
 
-			this->space_print(this->amount_space);
-			std::cout << "Test time: " << search_time/1000.0 << std::endl;
-			this->set_color(STANDART);
-			return error;
-		}
+			/*
+				@desc - Ставит нужное кол-во пробелов.
 
-
-	public:
-		CorsacTest(std::string name, int amount_space = 0){
-			this->name_block = name;
-			this->amount_space = amount_space;
-		};
-
-		int amount_test(){
-			int sum = this->tests.size();
-			for (int i = 0; i < this->blocks.size(); ++i)
-			{
-				sum += this->blocks[i].amount_test(); 
-			}
-
-			return sum;
-		}
-
-		void set_comment_color(int color)
-		{
-			this->comment_color = color;
-		}
-
-		void add_block(std::string name, std::function<void(CorsacTest*)> func_block)
-		{
-			func_block(this->create_block(name));		
-		}
-
-		CorsacTest*  add(std::string name, std::function<bool()> test)
-		{
-			this->tests.push_back(CorsacBlock(name, test));
-		
-			return this;
-		}
-
-		template <typename T, typename C>
-		CorsacTest* equal_ref(std::string name, T* f, C* t){
-			this->add(name, [=]()mutable
-			{
-				if(f == t)
-					return true;
-
-				return false;
-			});
-
-			return this;
-		}
-
-		template <typename T, typename C>
-		CorsacTest* equal_type(std::string name, T f, C t){
-			this->add(name, [=]()mutable
-			{
-				if(typeid(f).name() == typeid(t).name())
-					return true;
-
-				return false;
-			});
-
-			return this;
-		}
-
-		template <typename T, typename C>
-		CorsacTest* equal(std::string name, T f, C t){
-			this->add(name, [=]()mutable
-			{
-				if(f == t)
-					return true;
-
-				return false;
-			});
-
-			return this;
-		}
-
-		CorsacTest*  nt()
-		{
-			auto condition = this->tests[this->tests.size()-1].test;
-			this->tests[this->tests.size()-1].test = [=]()mutable{ return !condition();};
-		
-			return this;
-		}
-
-		template <typename T>
-		CorsacTest* is_false(std::string name, T f)
-		{
-			this->add(name, [=]()mutable
-			{
-				return f == false;
-			});
-			return this;
-		}
-
-		template <typename T>
-		CorsacTest* is_true(std::string name, T f)
-		{
-			this->add(name, [=]()mutable
-			{
-				return f == true;
-			});
-			return this;
-		}
-
-		template <typename T, typename C>
-		CorsacTest* is(std::string name, T f, C t)
-		{
-			this->add(name, [=]()mutable
-			{
-				if(typeid(f).name() == typeid(t).name())
-					return true;
-
-				return false;
-			});
-
-			return this;
-		}
-
-
-		/**
-		*	Проверка на наличие элемента в массиве
-		*	 
-		*	@param name		- Имя теста
-		*	@param start 	- Итератор начала массива
-		*	@param end 		- Итератор конца массива
-		*	@paramsearch 	- Элемент который ищется в массиве  
-		*/
-		template <typename iter, typename T>
-		CorsacTest* is_contein(std::string name, iter start, iter end, T search)
-		{
-			this->add(name, [=]()mutable
-			{
-				bool find = false;
-
-				for(; start != end; ++start)
+				@param amount - Кол-во пробелов.
+			*/		
+			void space_print(int amount){
+				for (int i = 0; i <= amount; i++)
 				{
-					if(*start == search){
-						find = true;
-						break;
+					std::cout << " ";
+				}
+			}
+
+			/*
+				@desc - Добавляет новый блок тестов.
+
+				@param name - Имя подблока.
+			*/		
+			Block* create_block(std::string name)
+			{
+				this->blocks.push_back(Block(name, this->amount_space+3));
+
+				return &this->blocks[this->blocks.size()-1];
+			}
+
+			/*
+				@desc - Устанавливает цвет текста.
+
+				@param color - Номер цвета.
+			*/		
+			void set_color(int color)
+			{
+
+				HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+				SetConsoleTextAttribute(hConsole, color);
+			}
+
+
+			/*
+				@desc - Запускает тесты и подтесты.
+			*/
+			int start_test()
+			{
+				unsigned int start_time = clock();
+
+				this->set_color(STANDART);
+				this->space_print(this->amount_space);
+				std::cout << this->name_block  << ": "<< std::endl;
+
+				int error = 0;
+
+				for(auto i = 0; i < this->tests.size(); i++)
+				{
+					auto cheked_test = this->tests[i];
+					auto result = cheked_test.test();
+
+					this->space_print(this->amount_space+3);
+
+					if(result){
+						this->set_color(GREEN);
+						std::cout << "\xfb:    ";
+					}
+					else{
+						this->set_color(RED);
+						std::cout << "X:    ";
+						error++;
+					}
+
+					std::cout << cheked_test.name <<std::endl;
+
+					if(cheked_test.comment.size() > 0){
+
+						this->space_print(this->amount_space+4);
+						this->set_color(cheked_test.comment_color);
+						std::cout << "> " << cheked_test.comment << std::endl;
 					}
 				}
-            	return find;
-			});	
-			return this;
-		}
 
-		void add_comment(std::string comment)
-		{
-			this->tests[this->tests.size()-1].comment = comment;
-			this->tests[this->tests.size()-1].comment_color = this->comment_color;
-		}
+				for(auto i = 0; i < this->blocks.size(); i++)
+				{
+					error += this->blocks[i].start_test();
+				}
 
-		void add_comment(std::string comment, int color)
-		{
-			this->tests[this->tests.size()-1].comment = comment;
-			this->tests[this->tests.size()-1].comment_color = color;
-		}
 
-		void start()
-		{
-			int error = this->start_test();
-			this->set_color(GREEN);
-			std::cout << " \xfb:    " << this->amount_test()-error << std::endl;
+				unsigned int end_time = clock(); 
+				unsigned int search_time = end_time - start_time; 
+				this->set_color(YELLOW);
+
+
+				this->space_print(this->amount_space);
+				std::cout << "Test time: " << search_time/1000.0 << std::endl;
+				this->set_color(STANDART);
+				return error;
+			}
+
+
+		public:
+			/*
+				@param name 	- Имя блока теста.
+				@amount_space 	- Задает кол-во отступов.
+			*/
+			Block(std::string name, int amount_space = 0){
+				this->name_block = name;
+				this->amount_space = amount_space;
+			};
+
+
+			/*
+				@desc - Возвращает кол-во тестов.
+			*/		
+			int amount_test(){
+				int sum = this->tests.size();
+				for (int i = 0; i < this->blocks.size(); ++i)
+				{
+					sum += this->blocks[i].amount_test(); 
+				}
+
+				return sum;
+			}
+
+			/*
+				@desc - Устонавливает базовый цвет текста комментария.
+
+				@param color - Номер цвета в консоли.
+			*/		
+			void set_comment_color(int color)
+			{
+				this->comment_color = color;
+			}
+
+
+			/*
+				@desc - Добавляет под блок для тестирования.
+
+				@note - Может использовать макрос TestBlock для сокращения записи, 
+						для этого надо прописать #define TEST_BLOCK.
+				@exemple - assert.add_block("Block1", TestBlock{ 
+								assert.equal("Equal", 1, 1);
+							});
+
+
+				@param name - Имя теста.
+				@param test - Лямбда функция с набором тестом.
+			*/		
+			void add_block(std::string name, std::function<void(Block*)> func_block)
+			{
+				func_block(this->create_block(name));		
+			}
+
+
+			/*
+				@desc - Добавляет кастомый тест.
+
+				@note - функция должна возвращать булево значение.
+				@note - Может использовать макрос Test для сокращения записи, 
+						для этого надо прописать #define TEST_ENABLE.
+				@exemple - assert.add("custom test", Test{ 
+								...code
+							});
+
+
+				@param name - Имя теста.
+				@param test - Лямбда функция с тестом, возвращает булево.
+			*/
+			Block*  add(std::string name, std::function<bool()> test)
+			{
+				this->tests.push_back(Test(name, test));
 			
-			this->set_color(RED);
-			std::cout << " X:    " << error << std::endl;
+				return this;
+			}
 
-			this->set_color(STANDART);
-		}
-		
+			/*
+				@desc - Сравнивает ссылку двух аргументов.
+
+				@param name 	- Имя теста.
+				@param f 		- Ссылка на первый аргумент.
+				@param t 		- Ссылка на второй аргумент.
+			*/
+			template <typename T, typename C>
+			Block* equal_ref(std::string name, T* f, C* t){
+				this->add(name, [=]()mutable
+				{
+					if(f == t)
+						return true;
+
+					return false;
+				});
+
+				return this;
+			}
+
+			/*
+				@desc - Сравнивает два аргумента.
+
+				@note - Тип аргументов должен иметь перегрузку, чтобы их можно было сравнить
+
+				@param name 	- Имя теста.
+				@param f 		- Первый сравниваемый аргумент.
+				@param t 		- Второй сравниваемый аргумент.
+			*/	
+			template <typename T, typename C>
+			Block* equal(std::string name, T f, C t){
+				this->add(name, [=]()mutable
+				{
+					if(f == t)
+						return true;
+
+					return false;
+				});
+
+				return this;
+			}
+
+			/*
+				@desc - Инвертирует ожидаемый результат теста.
+			*/	
+			Block* nt()
+			{
+				auto condition = this->tests[this->tests.size()-1].test;
+				this->tests[this->tests.size()-1].test = [=]()mutable{ return !condition();};
+			
+				return this;
+			}
+
+			/*
+				@desc - Проверяет на ложь.
+
+				@param name 	- Имя теста.
+				@param f 		- Проверяемый аргумент.
+			*/	
+			template <typename T>
+			Block* is_false(std::string name, T f)
+			{
+				this->add(name, [=]()mutable
+				{
+					return f == false;
+				});
+				return this;
+			}
+
+			/*
+				@desc - Проверяет на правду.
+
+				@param name 	- Имя теста.
+				@param f 		- Проверяемый аргумент.
+			*/		
+			template <typename T>
+			Block* is_true(std::string name, T f)
+			{
+				this->add(name, [=]()mutable
+				{
+					return f == true;
+				});
+				return this;
+			}
+
+			/*
+				@desc - Сравнивает типы элементов.
+
+				@param name 	- Имя теста.
+				@param f 		- Первый аргумент.
+				@param t 		- Второй аргумент с которым надо сравнить.
+			*/
+			template <typename T, typename C>
+			Block* is(std::string name, T f, C t)
+			{
+				this->add(name, [=]()mutable
+				{
+					if(typeid(f).name() == typeid(t).name())
+						return true;
+
+					return false;
+				});
+
+				return this;
+			}
+
+
+			/*
+				@desc - Проверяет контеинер на наличие в нем переданного элемента.
+
+				@param name		- Имя теста.
+				@param start 	- Итератор начала массива.
+				@param end 		- Итератор конца массива.
+				@param search 	- Элемент который ищется в массиве.  
+			*/
+			template <typename iter, typename T>
+			Block* is_contein(std::string name, iter start, iter end, T search)
+			{
+				this->add(name, [=]()mutable
+				{
+					bool find = false;
+
+					for(; start != end; ++start)
+					{
+						if(*start == search){
+							find = true;
+							break;
+						}
+					}
+	            	return find;
+				});	
+				return this;
+			}
+
+
+			/*
+				@desc - Добавляет комментарий после теста.
+
+				@param comment - Имя теста.  
+			*/
+			void add_comment(std::string comment)
+			{
+				this->tests[this->tests.size()-1].comment = comment;
+				this->tests[this->tests.size()-1].comment_color = this->comment_color;
+			}
+
+			/*
+				@desc - Добавляет комментарий после теста.
+
+				@param comment 	- Имя теста.
+				@param color 	- Получает номер цвета текста.  
+			*/
+			void add_comment(std::string comment, int color)
+			{
+				this->tests[this->tests.size()-1].comment = comment;
+				this->tests[this->tests.size()-1].comment_color = color;
+			}
+
+
+			/*
+				@desc - Запускает тесты и подтесты.
+			*/		
+			void start()
+			{
+				int error = this->start_test();
+				this->set_color(GREEN);
+				std::cout << " \xfb:    " << this->amount_test()-error << std::endl;
+				
+				this->set_color(RED);
+				std::cout << " X:    " << error << std::endl;
+
+				this->set_color(STANDART);
+			}
+			
 };
-
+}
 #endif
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,31 +1,31 @@
 #define TEST_BLOCK
 #include "Test.h"
 
+using namespace Corsac;
 
 int main()
 {
-	auto t = CorsacTest("Chek");
+	auto assert = Block("Global Test Block");
 
-	t.set_comment_color(RED);
+	assert.set_comment_color(YELLOW);
 
-	t.equal("test0", 0, 1);
-	t.equal("test0", 0, 1);
-	t.equal("test0", 0, 1);
-	t.equal("test0", 0, 1);
-	t.equal("test2", 0, 1)->nt();
-	t.equal("asdsad", 1, 1)->add_comment("Chek Comment");
-	t.equal("test0", 1, 1);
-	t.equal("test0", 1, 1);
+	assert.equal("test0", 0, 1);
+	assert.equal("test1", 0, 1);
+	assert.equal("test2", 0, 1);
+	assert.equal("test3", 0, 1);
+	assert.equal("test4", 0, 1)->nt();
+	assert.equal("test5", 1, 1)->add_comment("Chek Comment");
+	assert.equal("test6", 1, 1)->add_comment("Chek Castom color", STANDART);
+	assert.equal("test7", 1, 1);
 
-	t.add_block("test1", TestBlock{
-		assert->equal("1 and 1", 1, 1);
-		assert->equal("1 and 1", 1, 0);
-
-		assert->equal("1 and 1", 1, 0);
+	assert.add_block("Test Block 1", TestBlock{
+		assert->equal("test8", 1, 1);
+		assert->equal("test9", 1, 0);
+		assert->equal("test10", 1, 0);
 	});
 
 
-	t.start();
+	assert.start();
 
 
 	return 0;
